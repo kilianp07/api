b9c4737a39a5c157cac986e81d5e9e68ea8fb77f
kilianp07 <kilianp07@icloud.com>
2022-10-04 12:36:37 +0200

    Change entity relation

--- src/Controller/RecetteController.php
+++ src/Controller/RecetteController.php
@@ -11,6 +11,9 @@ use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Serializer\SerializerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use App\Entity\Recette;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\Entity;
 use Symfony\Component\Serializer\Annotation\Groups;
 
 class RecetteController extends AbstractController
@@ -23,8 +26,14 @@ class RecetteController extends AbstractController
         ]);
     }
     
-    #[Groups(["getAll"])]
     #[Route('/recette/getAll', name: 'cours.getAll')]
+    /**
+     * Return all recettes
+     *
+     * @param RecetteRepository $repository
+     * @param SerializerInterface $serializer
+     * @return JsonResponse
+     */
     public function getAllRecette(RecetteRepository $repository, SerializerInterface $serializer): JsonResponse
     {
         $recette = $repository->findAll();
@@ -45,13 +54,36 @@ class RecetteController extends AbstractController
     }
      */
     
-    #[Groups(["get"])]
-    #[Route('/recette/{id}', name: 'cours.get', methods: ['GET'])]
+    #[Route('/recette/{id}', name: 'recette.get', methods: ['GET'])]
     #[ParamConverter("recette",options:["id"=> "id"])]
+    /**
+     * Undocumented function
+     *
+     * @param Recette $recette
+     * @param RecetteRepository $repository
+     * @param SerializerInterface $serializer
+     * @return JsonResponse
+     */
     public function getOne(Recette $recette, RecetteRepository $repository, SerializerInterface $serializer): JsonResponse
     {
         $recette = $repository->find($recette);
         $jsonRecette = $serializer->serialize($recette, 'json');
         return New JsonResponse($jsonRecette,Response::HTTP_OK, ['accept'=>'json'],true);
+    }
+    
+    #[Route('/recette/{id}', name: 'recette.delete', methods: ['DELETE'])]
+    #[ParamConverter("recette",options:["id"=> "id"])]
+    /**
+     * This function delete the recette that is associated to id
+     *
+     * @param [type] $recette
+     * @param EntityManagerInterface $entityManager
+     * @return JsonResponse
+     */
+    public function deleteRecette($recette, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $entityManager->remove($recette);
+        $entityManager->flush();
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
     }
 }
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -31,8 +31,6 @@ use App\Entity\Recette;
           $manager -> persist($recette);
       }
      }
-
-
   }
 
   
